881578bdb2c634cc90b20c9218478d391277c8e8
Israel Cena <israelsjm@gmail.com>
2022-02-07 21:48:00 -0300

    setup project

--- client/cWeb/src/App.jsx
+++ client/cWeb/src/App.jsx
@@ -1,11 +1,15 @@
-const App = () => {
+import { Navbar, Footer, Loader, Services, Transactions, Welcome } from "./components"
+
+export default function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }
-
-export default App
